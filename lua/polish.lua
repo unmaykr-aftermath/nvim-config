fb0beb4ce307952663e5aba6be3260421dd3d453
unmaykr-aftermath <98741738+unmaykr-aftermath@users.noreply.github.com>
2024-06-16 07:48:25 +0200

    initial setup with rust pack

---
+++ lua/polish.lua
@@ -0,0 +1,36 @@
+-- if true then return end -- REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- This will run last in the setup process and is a good place to configure
+-- things like custom filetypes. This just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
+
+-- https://neovide.dev/faq.html
+if vim.g.neovide == true then
+  vim.g.neovide_scale_factor = 0.55
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-+>",
+    ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.05<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-->",
+    ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.05<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 0.55<CR>", { silent = true })
+end
+
+-- Set up custom filetypes
+-- vim.filetype.add {
+--   extension = {
+--     foo = "fooscript",
+--   },
+--   filename = {
+--     ["Foofile"] = "fooscript",
+--   },
+--   pattern = {
+--     ["~/%.config/foo/.*"] = "fooscript",
+--   },
+-- }
