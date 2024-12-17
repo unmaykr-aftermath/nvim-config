11da7d032093ab919f7a5ef0ad08bef6f6cd2536
unmaykr (aftermath) <98741738+unmaykr-aftermath@users.noreply.github.com>
2024-12-17 07:32:52 -0300

    feat(rust-analyzer-cfg): enable cargo.allFeatures
    Also reorganize configs

--- lua/plugins/rust-analyzer-cfg.lua
+++ lua/plugins/rust-analyzer-cfg.lua
@@ -2,15 +2,21 @@ return {
   {
     "AstroNvim/astrolsp",
     opts = function(_, opts)
-      opts.config.rust_analyzer.settings["rust-analyzer"].rustfmt = {
-        extraArgs = {
-          "+nightly",
+      opts.config.rust_analyzer.settings["rust-analyzer"] = {
+        cargo = {
+          allFeatures = true,
         },
-      }
 
-      opts.config.rust_analyzer.settings["rust-analyzer"].inlayHints = {
-        ignoreTraits = {
-          "*::OwoColorize",
+        rustfmt = {
+          extraArgs = {
+            "+nightly",
+          },
+        },
+
+        inlayHints = {
+          ignoreTraits = {
+            "*::OwoColorize",
+          },
         },
       }
 
