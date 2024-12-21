91ac14a8dfd8f640579b3a4ac855fbd99fde13be
unmaykr (aftermath) <98741738+unmaykr-aftermath@users.noreply.github.com>
2024-12-21 08:47:59 -0300

    docs(plugins): add note on markdown-preview fix

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,5 +1,6 @@
 return {
   {
+    -- Be sure to see: https://github.com/iamcco/markdown-preview.nvim/issues/695
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = "cd app && yarn install",
